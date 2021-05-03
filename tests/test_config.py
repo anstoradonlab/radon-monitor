42982a6e3ea30233ae7ac7e89ed2ce8298410715
Alan Griffiths <algriff@gmail.com>
2021-05-03 13:30:50 +1000

    Add tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+import logging
 
 import json
 from ansto_radon_monitor.configuration import (
@@ -29,24 +30,44 @@ raw_cfg = json.loads(
 
 
 def test_parse_config():
-
     config1 = parse_config(raw_cfg)
     assert config1.number_of_detectors == 2
-    assert config1.detector_config[0].kind == DetectorKind.L1500
+    assert config1.detector_config[0].kind == "L1500"
 
 
 def test_parse_command_line_args():
-    args = ["-vv", "--config=test.yaml"]
+    args = ["-vv", "--config=test.yaml", "run"]
     config = parse_args(args)
     assert config
     print(parse_args(args))
 
 
 def test_load_config_from_commandline():
-    args = ["-vv", "--config=__USE_DUMMY_INTERNAL_CONFIG"]
-    config = config_from_commandline(args)
+    args = ["-vv", "run"]
+    config, cmdline_args = config_from_commandline(args, raw_cfg=raw_cfg)
     assert config.loglevel == logging.DEBUG
 
-    args = ["--config=__USE_DUMMY_INTERNAL_CONFIG"]
-    config = config_from_commandline(args)
+    args = ["run"]
+    config, cmdline_args = config_from_commandline(args, raw_cfg=raw_cfg)
     assert config.loglevel == logging.ERROR
+
+
+def test_fg_flag():
+    args = ["-fg", "run"]
+    config, cmdline_args = config_from_commandline(args, raw_cfg=raw_cfg)
+    assert config.foreground == True
+
+    cfg_check = {
+        "number_of_detectors": "2",
+        "detector_config": [
+            {"name": "low", "port": "/dev/ttyS0", "kind": "L1500"},
+            {"name": "high", "port": "/dev/ttyS1", "kind": "L1500"},
+        ],
+        "foreground": "False",
+    }
+    config = parse_config(cfg_check)
+    assert config.foreground == False
+
+    args = ["run"]
+    config, cmdline_args = config_from_commandline(args, raw_cfg=raw_cfg)
+    assert config.foreground == False
---
+++ tests/test_main_controller.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+# WARNING: this test needs the logger and labjack to be plugged in!
+
+import pytest
+import logging
+import time
+import yaml
+import tempfile
+from ansto_radon_monitor.configuration import (
+    parse_config,
+    parse_args,
+    config_from_commandline,
+    DetectorKind,
+)
+
+from ansto_radon_monitor.main_controller import MainController, initialize
+
+
+__author__ = "Alan Griffiths"
+__copyright__ = "Alan Griffiths"
+__license__ = "mit"
+
+
+import json
+
+raw_cfg = yaml.safe_load(
+    """
+detector_config:
+    - detector_kind: "L1500"
+      serial_port: "serial:/dev/ttyUSB0:115200"
+      name: "TEST-002M"
+"""
+)
+
+
+@pytest.fixture(scope="session")
+def temp_directory():
+    print("Creating temp dir")
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        yield tmpdirname
+
+    print("removing temp dir")
+
+
+def test_main_controller(temp_directory):
+    print("testing in {}".format(temp_directory))
+    config, cmdline_args = config_from_commandline(["run"], raw_cfg)
+    print("initializing...")
+    ctl = initialize(config, mode="thread")
+    print("init done.")
+    time.sleep(1)
+    print(ctl.get_status())
+
+    time.sleep(10)
+
+    ctl.run_calibration()
+    ctl.stop_calibration()
+    ctl.run_background()
+    ctl.stop_background()
+    ctl.shutdown()
