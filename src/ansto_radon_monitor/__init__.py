f796cee14a16351bbf30112d0908a91736890075
Alan Griffiths <algriff@gmail.com>
2022-10-27 13:12:18 +1100

    Bump version info to 10.3

--- src/ansto_radon_monitor/__init__.py
+++ src/ansto_radon_monitor/__init__.py
@@ -15,6 +15,6 @@ try:
 except PackageNotFoundError:  # pragma: no cover
     # TODO: fix this properly using the PyInstaller Spec file
     # https://github.com/pyinstaller/pyinstaller/discussions/6033
-    __version__ = "10.2"
+    __version__ = "10.3"
 finally:
     del version, PackageNotFoundError
