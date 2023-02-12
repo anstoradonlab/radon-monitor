aa6933d0d30bdfa4619ce77a8357d1c38d2e0fc5
Alan Griffiths <Alan.Griffiths@ansto.gov.au>
2023-02-13 08:05:05 +1100

    Finish bumping version info to 10.5

--- src/ansto_radon_monitor/__init__.py
+++ src/ansto_radon_monitor/__init__.py
@@ -15,6 +15,6 @@ try:
 except PackageNotFoundError:  # pragma: no cover
     # TODO: fix this properly using the PyInstaller Spec file
     # https://github.com/pyinstaller/pyinstaller/discussions/6033
-    __version__ = "10.4"
+    __version__ = "10.5"
 finally:
     del version, PackageNotFoundError
