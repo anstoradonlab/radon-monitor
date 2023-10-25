bae9f1448a78f31af672acc4a18eabc9329974b3
Alan Griffiths <Alan.Griffiths@ansto.gov.au>
2023-10-25 20:36:03 +1100

    Set application icon

--- src/ansto_radon_monitor/gui/main.py
+++ src/ansto_radon_monitor/gui/main.py
@@ -1,14 +1,26 @@
 import logging
 import sys
+import os
 
 from ansto_radon_monitor.configuration import setup_logging
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
 from .mainwindow import MainWindow
 
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 def main():
     print("Starting GUI")
     setup_logging(loglevel=logging.DEBUG)
+
+    # this helps the taskbar icon to be grouped properly
+    # ref: https://www.pythonguis.com/tutorials/packaging-pyqt5-pyside2-applications-windows-pyinstaller/
+    try:
+        from ctypes import windll  # Only exists on Windows.
+        myappid = 'ansto.rdm.rdm.10'
+        windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+    except ImportError:
+        pass
+
 
     # Ref for this idea: https://stackoverflow.com/questions/8786136/pyqt-how-to-detect-and-close-ui-if-its-already-running
     lockfile_path = QtCore.QDir.tempPath() + "/ansto_radon_monitor_gui.lock"
@@ -16,6 +28,11 @@ def main():
 
     if lockfile.tryLock(100):
         app = QtWidgets.QApplication(sys.argv)
+        try:
+            # set the icon, but don't panic if it's not possible
+            app.setWindowIcon(QtGui.QIcon(os.path.join(basedir, 'Icon.ico')))
+        except:
+            pass
         window = MainWindow()
         window.show()
         exit_code = app.exec_()
