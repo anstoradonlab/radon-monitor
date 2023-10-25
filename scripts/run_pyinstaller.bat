d741c7b146aca5ac4548482dee8d52a511cf52f9
Alan Griffiths <Alan.Griffiths@ansto.gov.au>
2023-10-25 11:16:54 +1100

    Use spec file to control pyinstaller

--- scripts/run_pyinstaller.bat
+++ scripts/run_pyinstaller.bat
@@ -1,14 +1,16 @@
 REM TODO: update version info file before running pyinstaller
 
-pyinstaller --name RDM --noupx --log-level INFO --noconfirm ^
-  --icon ..\..\icons\Icon.ico ^
-  --version-file ..\..\scripts\file_version_info.txt ^
-  --distpath ..\dist ^
-  --specpath ..\dist\PyInstaller ^
-  --workpath ..\dist\PyInstaller ^
-  --add-binary ..\..\resources\windows\ljackuw.dll;. ^
-  ..\src\ansto_radon_monitor\main.py
+@REM spec file was generated with something like this command line
+@REM pyinstaller --name RDM --noupx --log-level INFO --noconfirm ^
+@REM   --icon ..\..\icons\Icon.ico ^
+@REM   --version-file ..\..\scripts\file_version_info.txt ^
+@REM   --distpath ..\dist ^
+@REM   --specpath ..\dist\PyInstaller ^
+@REM   --workpath ..\dist\PyInstaller ^
+@REM   --add-binary ..\..\resources\windows\ljackuw.dll;. ^
+@REM   ..\src\ansto_radon_monitor\main.py
 
-rem  --debug all ^
 
-
+cd ..
+pyinstaller --noconfirm src\RDM.spec
+cd scripts
